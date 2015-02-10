1aeddc07bd540da0d1e7a71366224b80c7dd6bfc
Trygve <trygve@lorentzenconsult.com>
2015-02-10 11:51:05 +0100

    Removed signing keys and removed old dll's

--- src/SharpArch/SharpArch.Web.Castle/WindsorExtensions.cs
+++ src/SharpArch/SharpArch.Web.Castle/WindsorExtensions.cs
@@ -12,28 +12,28 @@ namespace SharpArch.Web.Castle
 
     public static class WindsorExtensions
     {
-        /// <summary>
-        /// Searches for the first interface found associated with the 
-        /// <see cref="ServiceDescriptor" /> which is not generic and which 
-        /// is found in the specified namespace.
-        /// </summary>
-        public static BasedOnDescriptor FirstNonGenericCoreInterface(this ServiceDescriptor descriptor, string interfaceNamespace)
-        {
-            return descriptor.Select(
-                delegate(Type type, Type[] baseType)
-                {
-                    var interfaces =
-                        type.GetInterfaces().Where(
-                            t => t.IsGenericType == false && t.Namespace.StartsWith(interfaceNamespace));
+        ///// <summary>
+        ///// Searches for the first interface found associated with the 
+        ///// <see cref="ServiceDescriptor" /> which is not generic and which 
+        ///// is found in the specified namespace.
+        ///// </summary>
+        //public static BasedOnDescriptor FirstNonGenericCoreInterface(this ServiceDescriptor descriptor, string interfaceNamespace)
+        //{
+        //    return descriptor.Select(
+        //        delegate(Type type, Type[] baseType)
+        //        {
+        //            var interfaces =
+        //                type.GetInterfaces().Where(
+        //                    t => t.IsGenericType == false && t.Namespace.StartsWith(interfaceNamespace));
 
-                    if (interfaces.Count() > 0)
-                    {
-                        return new[] { interfaces.ElementAt(0) };
-                    }
+        //            if (interfaces.Count() > 0)
+        //            {
+        //                return new[] { interfaces.ElementAt(0) };
+        //            }
 
-                    return null;
-                });
-        }
+        //            return null;
+        //        });
+        //}
 
         public static IWindsorContainer RegisterController<T>(this IWindsorContainer container) where T : IController
         {
